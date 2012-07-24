60ebef94910cdf852c8018d63ef71e0102ad1419
Andrews Medina <andrewsmedina@gmail.com>
2012-07-24 15:02:20 -0300

    exempting csrf

--- fakeapi/api/views.py
+++ fakeapi/api/views.py
@@ -1,22 +1,27 @@
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def add_instance(request):
     return HttpResponse(status=201)
 
 
+@csrf_exempt
 @require_http_methods(["DELETE"])
 def remove_instance(request, name):
     return HttpResponse()
 
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def bind(request, name):
     return HttpResponse(status=201)
 
 
+@csrf_exempt
 @require_http_methods(["DELETE"])
 def unbind(request, name, host):
     return HttpResponse()
