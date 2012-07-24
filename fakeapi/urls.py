d95cfd2e1a7955e35747246bc401a89ff1b095fd
Andrews Medina <andrewsmedina@gmail.com>
2012-07-24 15:15:36 -0300

    url for status view

--- fakeapi/urls.py
+++ fakeapi/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^resources/$', 'fakeapi.api.views.add_instance'),  # post
     url(r'^resources/(?P<name>[\w-]+)/$', 'fakeapi.api.views.bind_or_remove_instance'),  # post and delete
+    url(r'^resources/(?P<name>[\w-]+)/status/$', 'fakeapi.api.views.status'),  # get
     url(r'^resources/(?P<name>[\w-]+)/host/(?P<host>[\w.]+)/$', 'fakeapi.api.views.unbind'),  # delete
 )
