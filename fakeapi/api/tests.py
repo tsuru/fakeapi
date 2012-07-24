2facedd66e261f3cad9eddfaef68d0b9b71a9136
Andrews Medina <andrewsmedina@gmail.com>
2012-07-24 14:28:23 -0300

    remove instance requires http delete method

--- fakeapi/api/tests.py
+++ fakeapi/api/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.test.client import RequestFactory
 
 
-from fakeapi.api.views import add_instance
+from fakeapi.api.views import add_instance, remove_instance
 
 
 class AddInstanceTestCase(TestCase):
@@ -23,3 +23,18 @@ class AddInstanceTestCase(TestCase):
         request = RequestFactory().post("/", {"name": "plains_of_dawn"})
         response = add_instance(request)
         self.assertEqual(201, response.status_code)
+
+
+class RemoveInstanceTestCase(TestCase):
+    def test_remove_instance_should_returns_405_when_method_is_not_delete(self):
+        request = RequestFactory().get("/")
+        response = remove_instance(request)
+        self.assertEqual(405, response.status_code)
+
+        request = RequestFactory().put("/")
+        response = remove_instance(request)
+        self.assertEqual(405, response.status_code)
+
+        request = RequestFactory().post("/")
+        response = remove_instance(request)
+        self.assertEqual(405, response.status_code)
--- fakeapi/api/views.py
+++ fakeapi/api/views.py
@@ -5,3 +5,8 @@ from django.views.decorators.http import require_http_methods
 @require_http_methods(["POST"])
 def add_instance(request):
     return HttpResponse("", status=201)
+
+
+@require_http_methods(["DELETE"])
+def remove_instance(request):
+    pass
